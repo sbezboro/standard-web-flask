318d4ee9a97420cda6a33ebece3b8aa8c9faee83
sbezboro <sergei.b.ru@gmail.com>
2014-02-03 22:53:38 -0800

    fix static urls

--- minify.sh
+++ minify.sh
@@ -1,17 +1,18 @@
-uglifyjs standardweb/static/js/jquery-1.8.3.js\
-    standardweb/static/js/jquery.flot.js\
-    standardweb/static/js/jquery.markitup.js\
-    standardweb/static/js/jquery.placeholder.js\
-    standardweb/static/js/jquery.timeago.js\
-    standardweb/static/js/jquery.tipsy.js\
-    standardweb/static/js/markitup.board.js\
-    standardweb/static/js/markitup.set.js\
-    standardweb/static/js/soundmanager2.js\
-    standardweb/static/js/ZeroClipboard.js\
-    standardweb/static/js/graph.js\
-    standardweb/static/js/streams.js\
-    standardweb/static/js/util.js\
-    --source-map standardweb/static/js/all.min.map\
+cd standardweb
+uglifyjs static/js/jquery-1.8.3.js\
+    static/js/jquery.flot.js\
+    static/js/jquery.markitup.js\
+    static/js/jquery.placeholder.js\
+    static/js/jquery.timeago.js\
+    static/js/jquery.tipsy.js\
+    static/js/markitup.board.js\
+    static/js/markitup.set.js\
+    static/js/soundmanager2.js\
+    static/js/ZeroClipboard.js\
+    static/js/graph.js\
+    static/js/streams.js\
+    static/js/util.js\
+    --source-map static/js/all.min.map\
     --source-map-url all.min.map\
-    --output standardweb/static/js/all.min.js\
-    -m
+    --output static/js/all.min.js\
+    --mangle
