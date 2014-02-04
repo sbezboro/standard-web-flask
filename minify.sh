352d07cbdf7d46e11952f22e56e758679c61e2b8
sbezboro <sergei.b.ru@gmail.com>
2014-02-03 22:43:30 -0800

    copy minify script over

---
+++ minify.sh
@@ -0,0 +1,17 @@
+uglifyjs standardweb/static/js/jquery-1.8.3.js\
+    standardweb/static/js/jquery.flot.js\
+    standardweb/static/js/jquery.markitup.js\
+    standardweb/static/js/jquery.placeholder.js\
+    standardweb/static/js/jquery.timeago.js\
+    standardweb/static/js/jquery.tipsy.js\
+    standardweb/static/js/markitup.board.js\
+    standardweb/static/js/markitup.set.js\
+    standardweb/static/js/soundmanager2.js\
+    standardweb/static/js/ZeroClipboard.js\
+    standardweb/static/js/graph.js\
+    standardweb/static/js/streams.js\
+    standardweb/static/js/util.js\
+    --source-map standardweb/static/js/all.min.map\
+    --source-map-url all.min.map\
+    --output standardweb/static/js/all.min.js\
+    -m
