e693bafedd5482bff8751e81121b4608056b03df
sbezboro <sergei.b.ru@gmail.com>
2014-02-03 21:57:02 -0800

    add debug toolbar

--- app.py
+++ app.py
@@ -2,4 +2,9 @@ from standardweb import app
 
 if __name__ == '__main__':
     app.config.from_object('settings')
+
+    if app.config['DEBUG']:
+        from flask_debugtoolbar import DebugToolbarExtension
+        DebugToolbarExtension(app)
+
     app.run()
