e82aeed180bcf54d05ca3e6425b1f676a106b0d3
sbezboro <sergei.b.ru@gmail.com>
2014-03-09 17:49:10 -0700

    listen on 0.0.0.0

--- app.py
+++ app.py
@@ -5,19 +5,14 @@ from werkzeug import script
 import sys
 
 
-def make_shell():
-    return {
-        'app': app
-    }
-
 if __name__ == '__main__':
     app.config.from_object('settings')
 
     if len(sys.argv) == 2 and sys.argv[1] == 'shell':
-        script.make_shell(make_shell, use_ipython=True)()
+        script.make_shell(lambda: {'app': app}, use_ipython=True)()
     else:
         if app.config['DEBUG']:
             from flask_debugtoolbar import DebugToolbarExtension
             DebugToolbarExtension(app)
 
-        app.run(threaded=True)
+        app.run(host='0.0.0.0', threaded=True)
