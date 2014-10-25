be196e29e7c2e7d0547fbb32a296ae5430c7cb16
sbezboro <sergei.b.ru@gmail.com>
2014-10-24 23:50:52 -0700

    move js around

--- minify.sh
+++ minify.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 cd standardweb
-uglifyjs static/js/jquery-1.8.3.min.js\
-    static/js/jquery.flot.min.js\
-    static/js/jquery.placeholder.min.js\
-    static/js/jquery.tipsy.min.js\
-    static/js/jquery.sceditor.min.js\
-    static/js/jquery.sceditor.bbcode.min.js\
-    static/js/moment.min.js\
-    static/js/soundmanager2.min.js\
-    static/js/ZeroClipboard.min.js\
-    static/js/site.js\
-    static/js/graph.js\
-    static/js/streams.js\
-    static/js/socket.io.min.js\
-    static/js/util.js\
+uglifyjs static/js/thirdparty/*.js\
+    static/js/local/*.js\
     --source-map static/js/all.min.map\
     --source-map-url all.min.map\
     --output static/js/all.min.js\
