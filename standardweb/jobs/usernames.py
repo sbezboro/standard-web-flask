930c1cccbe32b82c9e370af81554c6ae7fbdd391
sbezboro <sergei.b.ru@gmail.com>
2015-04-14 00:06:40 -0700

    Revert "distribute username check jobs over a week"
    This reverts commit 7ae137d5304f39cba36ab1c61e2c4919bc812635.

--- standardweb/jobs/usernames.py
+++ standardweb/jobs/usernames.py
@@ -15,10 +15,12 @@ def paged_query(query, limit=None):
         limit = 100
 
     while True:
-        rows = query.offset(offset).limit(limit)
+        rows = query.offset(offset).limit(limit).all()
 
-        for row in rows:
-            yield row
+        if not rows:
+            return
+
+        yield rows
 
         offset += limit
 
@@ -36,37 +38,58 @@ def schedule_checks():
         func.max(PlayerStats.last_seen) < datetime.utcnow() - timedelta(days=1)
     ).order_by(Player.id)
 
-    rollbar.report_message('Scheduling %d uuids for username change checks' % query.count(), level='info')
+    rollbar.report_message('Checking %d uuids for username changes' % query.count(), level='info')
 
-    # schedule a uuid check once every 6 seconds to spread across a week
-    for i, row in enumerate(paged_query(query)):
-        player_uuid = row.uuid
+    i = 0
+    # group uuid checks in groups of 100 every 100 seconds
+    for rows in paged_query(query):
+        player_uuids = [x.uuid for x in rows]
 
-        check_uuid.apply_async(
-            args=(player_uuid,),
-            countdown=i * 6
+        check_uuids.apply_async(
+            args=(player_uuids,),
+            countdown=i * 100
         )
+
+        i += 1
 
 
 @celery.task()
-def check_uuid(player_uuid):
-    player = Player.query.filter_by(uuid=player_uuid).first()
-    stats = PlayerStats.query.filter_by(
-        player=player, server_id=app.config.get('MAIN_SERVER_ID')
-    ).first()
+def check_uuids(player_uuids):
+    num_changed = 0
 
-    if stats and stats.last_seen > datetime.utcnow() - timedelta(days=1):
-        # ignore players that have joined since the job started
-        return
+    for uuid in player_uuids:
+        player = Player.query.filter_by(uuid=uuid).first()
+        stats = PlayerStats.query.filter_by(
+            player=player, server_id=app.config.get('MAIN_SERVER_ID')
+        ).first()
 
-    try:
-        actual_username = minecraft_uuid.lookup_latest_username_by_uuid(player_uuid)
-    except requests.RequestException:
-        return
+        if stats and stats.last_seen > datetime.utcnow() - timedelta(days=1):
+            # ignore players that have joined since the job started
+            continue
 
-    if not actual_username:
-        return
+        try:
+            actual_username = minecraft_uuid.lookup_latest_username_by_uuid(uuid)
+        except requests.RequestException as e:
+            rollbar.report_message('Exception looking up uuid, skipping group', level='warning', extra_data={
+                'num_changed': num_changed,
+                'exception': unicode(e)
+            })
+            return
 
-    if actual_username != player.username:
-        player.set_username(actual_username)
-        player.save(commit=True)
+        if not actual_username:
+            rollbar.report_message('Error getting actual username, skipping', level='warning', extra_data={
+                'uuid': uuid
+            })
+            continue
+
+        if actual_username != player.username:
+            player.set_username(actual_username)
+            player.save(commit=False)
+
+            num_changed += 1
+
+    db.session.commit()
+
+    rollbar.report_message('Finished checking uuid group', level='info', extra_data={
+        'num_changed': num_changed
+    })
