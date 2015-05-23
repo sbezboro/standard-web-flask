2b2789c9c1befcc9aab619bef6918ea12ee7e516
sbezboro <sergei.b.ru@gmail.com>
2015-05-23 11:44:06 -0700

    fix up external referrer blocking

--- standardweb/views/decorators/referrers.py
+++ standardweb/views/decorators/referrers.py
@@ -6,11 +6,20 @@ import rollbar
 from standardweb import app
 
 
+VALID_REFERRER_SUBSTRINGS = frozenset((
+    'standardsurvival.com', 'www.google.com'
+))
+
+
 def reject_external_referrers():
     def decorator(func):
         @wraps(func)
         def wrapped(*args, **kwargs):
-            if not app.config['DEBUG'] and 'standardsurvival.com' not in request.referrer:
+            if (
+                not app.config['DEBUG'] and
+                request.referrer and
+                not any(s in request.referrer for s in VALID_REFERRER_SUBSTRINGS)
+            ):
                 rollbar.report_message('External referrer blocked', level='warning', request=request)
                 abort(401)
 
