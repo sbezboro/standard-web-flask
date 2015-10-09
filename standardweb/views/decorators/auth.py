5875ddbdba965bd7f08f0d1436367b6efcb1118d
sbezboro <sergei.b.ru@gmail.com>
2015-10-08 23:31:12 -0700

    add moderator option for auth decorator

--- standardweb/views/decorators/auth.py
+++ standardweb/views/decorators/auth.py
@@ -3,7 +3,7 @@ from functools import wraps
 from flask import abort, flash, g, redirect, request, url_for
 
 
-def login_required(show_message=True, only_admin=False):
+def login_required(show_message=True, only_moderator=False, only_admin=False):
     """
     Decorator that will redirect the user to the login page
     if they aren't logged in. Raises a 403 if the view requires
@@ -23,6 +23,9 @@ def login_required(show_message=True, only_admin=False):
                 return redirect(url_for('login', next=next))
 
             if only_admin and not g.user.admin:
+                abort(403)
+
+            if only_moderator and not g.user.admin and not g.user.moderator:
                 abort(403)
 
             return func(*args, **kwargs)
