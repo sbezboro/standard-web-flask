dc2eb1690b036760a6026334d35a499ec39ba8fe
sbezboro <sergei.b.ru@gmail.com>
2019-01-02 21:23:35 -0800

    fix bad ansi codes coming from paperspigot server

--- standardweb/lib/helpers.py
+++ standardweb/lib/helpers.py
@@ -32,7 +32,37 @@ def safe_str_cmp(a, b):
     return rv == 0
 
 
+# temporary workaround
+# maps section sign codes to real ansi codes, as paperspigot doesn't seem to include jansi in the runtime
+_ansi_mc_map = {
+    u"\xa70": u"\x1B[0;30;22m", # Black 0
+    u"\xa71": u"\x1B[0;34;22m", # Dark Blue 1
+    u"\xa72": u"\x1B[0;32;22m", # Dark Green 2
+    u"\xa73": u"\x1B[0;36;22m", # Dark Aqua 3
+    u"\xa74": u"\x1B[0;31;22m", # Dark Red 4
+    u"\xa75": u"\x1B[0;35;22m", # Dark Purple 5
+    u"\xa76": u"\x1B[0;33;22m", # Gold 6
+    u"\xa77": u"\x1B[0;37;22m", # Gray 7
+    u"\xa78": u"\x1B[0;30;1m",  # Dark Gray 8
+    u"\xa79": u"\x1B[0;34;1m",  # Blue 9
+    u"\xa7a": u"\x1B[0;32;1m",  # Green a
+    u"\xa7b": u"\x1B[0;36;1m",  # Aqua b
+    u"\xa7c": u"\x1B[0;31;1m",  # Red c
+    u"\xa7d": u"\x1B[0;35;1m",  # Light Purple d
+    u"\xa7e": u"\x1B[0;33;1m",  # Yellow e
+    u"\xa7f": u"\x1B[0;37;1m",  # White f
+    u"\xa7k": u"\x1B[5m",       # Obfuscated k
+    u"\xa7l": u"\x1B[21m",      # Bold l
+    u"\xa7m": u"\x1B[9m",       # Strikethrough m
+    u"\xa7n": u"\x1B[4m",       # Underline n
+    u"\xa7o": u"\x1B[3m",       # Italic o
+    u"\xa7r": u"\x1B[39;0m",    # Reset r
+}
+
+
 def ansi_to_html(ansi):
+    for code in _ansi_mc_map:
+        ansi = ansi.replace(code, _ansi_mc_map[code])
     html = _ansi_converter.convert(ansi, full=False)
     count = html.count('<span') - html.count('</span')
     return '<span class="ansi-container">' + html + ('</span>' * count) + '</span>'
